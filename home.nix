bb07fee73a48aee79aed9f4d36813064a5932f1c
sergioahp <sergioahp@proton.me>
2025-01-23 21:06:39 -0600

    add keyboard speed and rate

--- home.nix
+++ home.nix
@@ -135,6 +135,8 @@
         kb_layout = "us";
         kb_variant = "altgr-intl";
         kb_options = "caps:swapescape";
+        repeat_delay = 250;
+        repeat_rate = 70;
         follow_mouse = 1;
         sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
         touchpad = {
