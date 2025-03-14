f3a49435ddd105f3ea11919269947423313930fd
sergioahp <sergioahp@proton.me>
2025-03-14 16:39:25 -0600

    add hyprlock

--- home.nix
+++ home.nix
@@ -319,6 +319,16 @@ in
   services.hyprpaper = {
     enable = false;
   };
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      general = {
+        disable_loading_bar = true;
+        hide_cursor = true;
+        grace = 300;
+      };
+    };
+  };
   services.dunst = {
     enable = true;
     settings = {
