f5030f8eb126bde20e6464a6bc95948550652e22
sergioahp <sergioahp@proton.me>
2026-06-21 07:56:58 -0600

    Add morphllm api key

--- home.nix
+++ home.nix
@@ -283,6 +283,7 @@ in
     secrets."personal-api-keys/OPENROUTER_API_KEY" = {};
     secrets."personal-api-keys/MISTRAL_API_KEY" = {};
     secrets."personal-api-keys/INCEPTION_API_KEY" = {};
+    secrets."personal-api-keys/MORPH_API_KEY" = {};
   };
 
   services.podman = {
